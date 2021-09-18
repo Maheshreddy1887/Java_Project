3a9aec8c933e2049fb26abd96235b285749455f6
mahesh m <maheshreddyreddy1887@gmail.com>
2021-09-18 12:15:24 -0400

    first commit

---
+++ final_keyword/src/com/xworkz/java/final_keyword/PowerBank.java
@@ -0,0 +1,12 @@
+package com.xworkz.java.final_keyword;
+
+public class PowerBank {
+	final static int capacity =2000;
+	
+	
+	public static int fhh(){
+		int copyCapacity= capacity;
+		
+		return copyCapacity;
+	}
+}
---
+++ final_keyword/src/com/xworkz/java/final_keyword/PowerBank_Tester.java
@@ -0,0 +1,14 @@
+package com.xworkz.java.final_keyword;
+
+public class PowerBank_Tester {
+
+	public static void main(String[] args) {
+	
+	//	PowerBank powerBank=new PowerBank();
+	      
+		System.out.println(PowerBank.fhh());
+		
+       
+	}
+
+}
